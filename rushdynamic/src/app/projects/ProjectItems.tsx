cff8e612948b11125dde973581c60900458b6b90
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-09-07 18:51:35 +0530

    feat: projects horizontal layout

--- rushdynamic/src/app/projects/ProjectItems.tsx
+++ rushdynamic/src/app/projects/ProjectItems.tsx
@@ -9,7 +9,7 @@ interface ProjectItemTagProps {
 }
 const ProjectItemTag = ({ tagName }: ProjectItemTagProps) => {
 	return (
-		<div className="w-fit flex py-0.5 px-2 rounded-xl text-xs text-light-grey border border-solid bg-dark-grey border-dark-grey">
+		<div className="w-fit flex items-center justify-center py-0.5 px-2 rounded-xl text-xs text-light-grey border border-solid bg-dark-grey border-dark-grey font-jetbrains-mono">
 			{tagName}
 		</div>
 	);
@@ -32,37 +32,36 @@ const ProjectItem = ({
 	git,
 }: ProjectItemProps) => {
 	return (
-		<Link href={git} passHref legacyBehavior>
-			<a target="_blank" rel="noopener noreferrer">
-				<motion.div
-					whileHover={{ scale: 1.05 }}
-					transition={{ type: 'spring', stiffness: 400, damping: 10 }}
-					className="group h-full w-50 min-w-50 lg:w-80 lg:min-w-80 p-6 bg-gradient-to-t from-white to-transparent flex flex-col gap-2 justify-between md:border-t border-solid border-dark-grey hover:cursor-pointer"
-				>
-					<div className="flex items-center justify-center max-w-[80]">
-						<Image
-							src={image}
-							className="rounded-sm"
-							alt={title}
-							height={280}
-							width={280}
-						/>
-					</div>
-					<div className="flex items-center justify-between gap-1">
-						<div className="text-4xl">{title}</div>
-						<div className="text-sm px-2 border border-solid border-dark-grey group-hover:bg-dark-grey group-hover:text-light-grey">
-							{year}
+		<div className="block w-full min-w-0">
+			<Link href={git} passHref legacyBehavior>
+				<a target="_blank" rel="noopener noreferrer">
+					<motion.div
+						whileHover={{ scale: 1.05 }}
+						transition={{ type: 'spring', stiffness: 400, damping: 25 }}
+						className="group h-full w-full min-w-50 lg:min-w-80 bg-gradient-to-r from-[#d2c9a0] to-[#d2c9a0] flex flex-col lg:flex-row lg:gap-8  hover:cursor-pointer items-stretch border border-solid border-dark-grey"
+					>
+						<div className="flex items-center justify-center p-6 bg-dark-grey min-w-fit">
+							<Image src={image} alt={title} height={280} width={280} />
 						</div>
-					</div>
-					<div className="text-lg">{description}</div>
-					<div className="flex flex-wrap gap-1">
-						{tags.map((tag) => (
-							<ProjectItemTag key={tag} tagName={tag} />
-						))}
-					</div>
-				</motion.div>
-			</a>
-		</Link>
+						<div className="flex flex-col p-6 w-full gap-3">
+							<div className="flex items-start justify-between gap-2 flex-col lg:flex-row lg:items-center">
+								<div className="text-4xl font-semibold">{title}</div>
+								<div className="text-sm px-2 border border-solid border-dark-grey group-hover:bg-dark-grey group-hover:text-light-grey">
+									{year}
+								</div>
+							</div>
+							<div className="text-lg">{description}</div>
+							<div className="grow" />
+							<div className="flex justify-end flex-wrap gap-1">
+								{tags.map((tag) => (
+									<ProjectItemTag key={tag} tagName={tag} />
+								))}
+							</div>
+						</div>
+					</motion.div>
+				</a>
+			</Link>
+		</div>
 	);
 };
 
@@ -71,7 +70,7 @@ interface ProjectItemsProps {
 }
 const ProjectItems = ({ projects }: ProjectItemsProps) => {
 	return (
-		<div className="w-full flex items-start justify-center gap-6 flex-wrap">
+		<div className="w-full flex items-start justify-center gap-6 flex-col">
 			{projects.map((project) => (
 				<ProjectItem
 					key={project.title}
