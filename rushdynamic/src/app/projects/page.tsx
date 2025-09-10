46491c16fc8dbcfedd3ea02a97cc43b7cbbb4a84
rushdynamic <tameimpalafanclubpresident@gmail.com>
2025-09-10 17:44:06 +0530

    feat: added desk img to projects page

--- rushdynamic/src/app/projects/page.tsx
+++ rushdynamic/src/app/projects/page.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 import projects from '../data/projects.json';
 import ProjectItems from './ProjectItems';
+import deskImage from '../../../public/images/desk.jpg';
 
 export default function Projects() {
 	return (
 		<div className="p-8 font-montreal text-dark-grey flex flex-col gap-4">
 			<div className="text-7xl font-semibold">Projects</div>
+			<Image
+				src={deskImage}
+				alt="desk"
+				className="rounded-xl hidden lg:block select-none border border-solid border-dark-grey border-2 my-4"
+			/>
 			<div className="text-xl md:text-2xl lg:text-3xl mb-8">
 				{
 					"Stuff I've built for learning purposes. And because it was fun. Mostly because it was fun."
 				}
 				<br />
-				<span className="text-xl lg:text-2xl">
+				<span className="text-sm md:text-xl lg:text-2xl">
 					{
 						'Fueled by curiosity, caffeine, and the occasional existential dread.'
 					}
